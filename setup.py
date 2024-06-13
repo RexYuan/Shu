c569c440b8b652f0ab25a4fe2dc2a0a017181d5b
RexYuan <r1218r1218@gmail.com>
2024-06-14 02:59:52 +0800

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("src/README.md", "r") as f:
 
 setup(
     name="fairness_checker",
-    version="0.0.5",
+    version="0.1.0",
     package_dir={'': 'src'},
     packages=find_packages(where="src"),
     description="Fairnes checker",
